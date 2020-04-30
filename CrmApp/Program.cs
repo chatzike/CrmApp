260e7a81d4092d4f6d7ebee70286be2d2f60fb01
Stylianos Xatzikexagias <sxatzikexagias@athtech.gr>
2020-04-30 13:24:06 +0300

    Add project files.

---
+++ CrmApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrmApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Ui ui = new Ui();
+            Customer customer = ui.CreateCustomer();
+            Basket basket = ui.CreateBasket();
+
+
+        }
+    }
+}
---
+++ CrmApp/Ui.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrmApp
+{
+
+
+    class Ui
+    {
+
+
+
+        //Factory method
+        public Product CreateProduct()
+        {
+            Product product = new Product();
+            //try-catch block to manage user's input
+            try
+            {
+
+                Console.WriteLine("Give the code ");
+                product.Code = Console.ReadLine();
+                Console.WriteLine("Give the name ");
+                product.Name = Console.ReadLine();
+                Console.WriteLine("Give the price ");
+                product.Price = Decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Give the quantity ");
+                product.Quantity = Int32.Parse(Console.ReadLine());
+
+                return product;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You have not completed the questions properly." +
+                    " Please try again.");
+                return null;
+            }
+
+        }
+
+        public int Menu()
+        {
+            Console.WriteLine("1.  Add a customer  2.Add a product   3. Display basket " +
+                " 4. ShowCategories  5. TotalCost 0. Exit");
+            Console.WriteLine("Give your choice");
+            int choice = 0;
+            try
+            {
+                choice = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+            }
+            return choice;
+        }
+
+
+        public Customer CreateCustomer()
+        {
+            return null;
+        }
+
+        public Basket CreateBasket()
+        {
+            Basket basket = new Basket();
+            int choice;
+
+            do
+            {
+                choice = Menu();
+                switch (choice)
+                {
+                    case 1:
+                        Customer customer = CreateCustomer();   //simplirose diergasia add customer
+                        break;
+                    case 2:
+                        Product product = CreateProduct();
+                        basket.AddProduct(product);
+                        break;
+                    case 3:
+                        basket.Print();
+                        break;
+                    case 4:
+                        basket.ShowCategories();
+                        break;
+                    case 5:
+                        Console.WriteLine("TotalCost= " + basket.TotalCost());
+                        break;
+                    case 0:
+                        Console.WriteLine("You selected to exit");
+                        break;
+
+                }
+            }
+            while (choice != 0);
+            return basket;
+        }
+
+    }
+}
